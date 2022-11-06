302d41404f876c6245c7249e5b0e57ec37363e92
Murali99 <ganjimurali101@gmail.com>
2022-11-06 05:27:37 +0000

    where i can do mistake can u explain

---
+++ ratioFactorial/index.js
@@ -0,0 +1,5 @@
+const factorialOfNumber = require("../utilities/factorial/index");
+const ratioOfTwoNumbers = require("../utilities/ratioFactorial/index");
+
+console.log(factorialOfNumber(6));
+console.log(ratioOfTwoNumbers(10, 5));
